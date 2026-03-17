aabb9e38f72dd55a3f05056ea777763cab292133
mohammmed999 <akeelalsaade8@gmail.com>
2026-03-17 18:04:56 +0300

    Update MainView.axaml.cs

--- DirectPackageInstaller/DirectPackageInstaller/Views/MainView.axaml.cs
+++ DirectPackageInstaller/DirectPackageInstaller/Views/MainView.axaml.cs
@@ -919,7 +919,7 @@ namespace DirectPackageInstaller.Views
         {
             if (string.IsNullOrWhiteSpace(App.Config.PSIP) || string.IsNullOrWhiteSpace(App.Config.PCIP))
             {
-                await MessageBox.ShowAsync(Parent, "IP .فشل العثور على عنوان الخاص بجهاز البلايستيشن\nيرجى كتابة عنوان PS/PC لجهاز IP في قائمة الخيارات", "DirectPackageInstaller", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                await MessageBox.ShowAsync(Parent, "IP فشل العثور على عنوان. الخاص بجهاز البلايستيشن\nIP يرجى كتابة عنوان في قائمة الخيارات PS/PC لجهاز", "DirectPackageInstaller", MessageBoxButtons.OK, MessageBoxIcon.Error);
                 return false;
             }
 
